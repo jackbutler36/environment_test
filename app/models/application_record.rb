ba14366c16f29d371618d0876aa2ecba2165d82a
root <root@localhost>
2022-09-15 08:07:43 +0000

    Trying step 7 Create

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,22 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
+
+#class AddAuthorColToBook < ActiveRecord::Migration[6.1]
+#  def change
+#    add_column :book, :author, :string
+#  end
+#end
+
+#class AddPriceColToBook < ActiveRecord::Migration[6.1]
+#  def change
+#    add_column :book, :price, :decimal
+#  end
+#end
+
+#class AddDateColToBook < ActiveRecord::Migration[6.1]
+#  def change
+#    add_column :book, :pub_date, :date
+#  end
+#end
+
